8eb79591796c306a87dfe04e69ee368368dc951b
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-11-18 09:46:51 +0300

    "Added code to delete existing data from database tables before seeding"

--- seed.py
+++ seed.py
@@ -3,5 +3,15 @@ from models import db, User, Admin, LostItem, FoundItem, Claim, Reward, RewardPa
 
 with app.app_context(): 
     
-   
-   
+    # This will delete any existing rows so you can run the seed file multiple times without having duplicate entries in your database
+    print("Deleting data ...")
+    RewardPayment.query.delete()
+    Claim.query.delete()
+    Comment.query.delete()
+    Reward.query.delete()
+    LostItem.query.delete()
+    FoundItem.query.delete()
+    User.query.delete()
+    Admin.query.delete()
+    
+    db.session.commit()
